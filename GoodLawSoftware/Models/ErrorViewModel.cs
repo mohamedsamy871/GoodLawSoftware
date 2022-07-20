722985b5199ff0f8df44ad889334382ffc7239a2
mohamedsamy871 <mohamedsamy871@gmail.com>
2022-07-20 10:51:45 +0200

    Add project files.

---
+++ GoodLawSoftware/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoodLawSoftware.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
