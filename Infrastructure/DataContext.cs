3c5761ea194d7625bc076040c5773b30c4e15dd1
mohamedsamy871 <mohamedsamy871@gmail.com>
2022-07-20 16:02:54 +0200

    adding core and infrastructure

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext()
+        {
+
+        }
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<PasswordItem>().Property(x => x.Id).HasDefaultValueSql("NEWID()");
+        }
+        public DbSet<PasswordItem> PasswordItem { get; set; }
+    }
+}
